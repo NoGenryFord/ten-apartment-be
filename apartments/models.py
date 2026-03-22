62f87182a632c20700a8350f0e1e88dff4a36b00
Stanislav Kucherenko <svkwork1@gmail.com>
2026-03-22 19:15:56 +0100

    add apartment, booking, schedule, and price models with API routing

--- apartments/models.py
+++ apartments/models.py
@@ -5,3 +5,30 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Schedule(models.Model):
+    date = models.DateField()
+    apartment = models.ForeignKey('apartments.Apartment', on_delete=models.CASCADE)
+    Price = models.ForeignKey('Price', on_delete=models.CASCADE)
+    is_booking = models.BooleanField()
+
+class Apartment(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.ForeignKey('apartments.ApartmentType', on_delete=models.CASCADE)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='apartments/')
+    video = models.FileField(upload_to='apartments/')
+
+class ApartmentType(models.Model):
+    name = models.CharField(max_length=100)
+
+class Price(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Booking(models.Model):
+    apartments = models.ForeignKey('apartments.Apartment', on_delete=models.CASCADE)
+    price = models.ForeignKey('Price', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+
+
--- ten_apartment_be/settings.py
+++ ten_apartment_be/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
@@ -137,4 +137,10 @@ CORS_ALLOWED_ORIGINS= [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
