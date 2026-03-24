11228d7d279f677606784e3768613f488c0ea934
Stanislav Kucherenko <svkwork1@gmail.com>
2026-03-24 13:43:46 +0100

    register additional models in admin panel

--- apartments/admin.py
+++ apartments/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from .models import Apartment
+from .models import Apartment, User, Schedule, Price, ApartmentType, Booking, BookingSlot
 
 # Register your models here.
 admin.site.register(Apartment)
+admin.site.register(User)
+admin.site.register(Schedule)
+admin.site.register(Price)
+admin.site.register(ApartmentType)
+admin.site.register(Booking)
+admin.site.register(BookingSlot)
+
