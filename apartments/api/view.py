c8e8accf98b457950b131beecf53e319082952b0
Stanislav Kucherenko <svkwork1@gmail.com>
2026-03-28 00:36:38 +0100

    enhance ApartmentSerializer by adding related photos, videos, and total_price fields

--- apartments/api/view.py
+++ apartments/api/view.py
@@ -1,13 +1,35 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
 
 from apartments.api.serializers import ApartmentSerializer, ScheduleSerializer
 from apartments.models import Apartment, Schedule
 
 
-class ApartmentViewSet(viewsets.ModelViewSet):
+class ApartmentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Apartment.objects.all()
     serializer_class = ApartmentSerializer
+    permission_classes = [permissions.AllowAny]
 
-class ScheduleViewSet(viewsets.ModelViewSet):
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        """
+        Эндпоинт: /api/apartments/search/?start_date=2026-01-01&end_date=2026-01-20
+        """
+        pass
+
+
+class ScheduleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        """
+        Календарь для конкретной квартиры
+        /api/schedules/?apartment_id=1
+        """
+        queryset = super().get_queryset()
+        apartment_id = self.request.query_params.get('apartment_id')
+        if apartment_id:
+            queryset = queryset.filter(apartment_id=apartment_id)
+        return queryset
