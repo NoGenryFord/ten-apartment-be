e54bea1250f9cbf730379a7e519c5cda01737b3f
Stanislav Kucherenko <svkwork1@gmail.com>
2026-03-27 17:53:15 +0100

    enhance ApartmentSerializer by adding related photos, videos, and total_price fields

--- apartments/api/serializers.py
+++ apartments/api/serializers.py
@@ -1,16 +1,49 @@
 from rest_framework import serializers
-from apartments.models import Apartment, Schedule
+from apartments.models import Apartment, Schedule, ApartmentPhoto, ApartmentVideo, ApartmentType
+
+class ApartmentPhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApartmentPhoto
+        fields = ['id', 'photo', 'order']
+
+
+class ApartmentVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApartmentVideo
+        fields = ['id', 'video', 'order']
+
+class ApartmentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApartmentType
+        fields = ['id', 'name']
 
 class ApartmentSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Квартира. Включает в себя связанные фото, видео и тип квартиры.\n
+    Атрибуты:
+    url_obj - для получения URL объекта, для удобства навигации в API. Например: /api/apartments/n/ \n
+    photos - используются только в detail просмотре объекта
+    videos - используются только в detail просмотре объекта
+    types - тип апартаментов, ссылка на модель типов апартаментов
+    total_price - Не обязательное поле, заполняется только в случае поиска по датам
+
+    """
     url_obj = serializers.HyperlinkedIdentityField(
         view_name='apartments-detail',
         lookup_field='pk'
     )
 
+    photos = ApartmentPhotoSerializer(many=True, read_only=True)
+    videos = ApartmentVideoSerializer(many=True, read_only=True)
+    type = ApartmentTypeSerializer(read_only=True)
+
+
+    # Не обязательное поле, заполняется только в случае поиска по датам
+    total_price = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True, required=False)
+
     class Meta:
         model = Apartment
-        fields = ['id', 'name', 'type', 'description', 'url_obj']
-        read_only_fields = ['id', 'name', 'type', 'description']
+        fields = ['id', 'name', 'type', 'description', 'photos', 'videos', 'total_price', 'url_obj']
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
@@ -19,9 +52,10 @@ class ScheduleSerializer(serializers.ModelSerializer):
         lookup_field='pk'
     )
 
+    price_value = serializers.DecimalField(source='price.price', max_digits=10, decimal_places=2, read_only=True)
+
     class Meta:
         model = Schedule
-        fields = ['id', 'date', 'apartment', 'price', 'status', 'url_obj']
-        read_only_fields = ['id', 'date', 'apartment', 'price', 'status']
+        fields = ['id', 'date', 'apartment', 'price_value', 'status', 'url_obj']
 
 
